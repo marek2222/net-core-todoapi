0483005b1cc6fe502dce11669798856be14272cb
marek2222 <marekmusial2@o2.pl>
2019-02-08 11:26:31 +0100

    net-core-todoapi:: 1_8:    Add a Create method

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -47,5 +47,16 @@ namespace net_core_todoapi.Controllers
     }
 
 
+    // Add a Create method
+    // POST: api/Todo
+    [HttpPost]
+    public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem item)
+    {
+      _context.TodoItems.Add(item);
+      await _context.SaveChangesAsync();
+
+      return CreatedAtAction(nameof(GetTodoItem), new { id = item.Id }, item);
+    }
+
   }
 }
