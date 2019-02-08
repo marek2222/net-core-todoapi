daa66b7a2bba5ab93c14c0bcbd904345231efdc2
marek2222 <marekmusial2@o2.pl>
2019-02-08 11:58:53 +0100

    net-core-todoapi: 1_10:    Add a DeleteTodoItem method

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -74,6 +74,20 @@ namespace net_core_todoapi.Controllers
     }
 
 
+    // DELETE: api/Todo/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteTodoItem(long id)
+    {
+      var todoItem = await _context.TodoItems.FindAsync(id);
+      if (todoItem == null)
+      {
+        return NotFound();
+      }
+      _context.TodoItems.Remove(todoItem);
+      await _context.SaveChangesAsync();
+
+      return NoContent();
+    }
 
 
   }
